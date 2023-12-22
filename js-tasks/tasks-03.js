3826b3f5cbb73546ee5d2b8aab15ec4792248c76
Hanna V <hanna.vishyou@gmail.com>
2023-12-22 17:04:51 +0300

    Update tasks-03 + doll.js

--- js-tasks/tasks-03.js
+++ js-tasks/tasks-03.js
@@ -61,3 +61,19 @@ class Dog extends Animal {
         return `Hello ${this.master}`;
     }
 }
+
+// https://www.codewars.com/kata/5721c189cdd71194c1000b9b
+function grabDoll(dolls) {
+  var bag = [];
+  for (var i = 0; i < dolls.length; i++) {
+    if (dolls[i] === "Hello Kitty" || dolls[i] === "Barbie doll") {
+      bag.push(dolls[i]);
+    }
+    if (bag.length !== 3) {
+      continue;
+    } else {
+      break;
+    }
+  }
+  return bag;
+}
