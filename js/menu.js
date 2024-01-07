b9703d41547c522e6fc5b2ae7c2cacbb4d029cf2
Yullia <julligurska05@gmail.com>
2024-01-07 16:47:21 +0200

    snacks json

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,7 @@
 document.addEventListener('partialsLoaded',
 () => {
 import('./burger-menu.js');
+import('./menu/food-slide.js');
 import('./menu/bar-slides.js');
 import('./menu/food-slides.js');
 });
--- js/testimonials.js
+++ js/testimonials.js
@@ -1,25 +1,10 @@
-const testimonials =[
-    {
-        id: '1',
-        image: 'img/testimonials/profile-sam.png',
-        name: 'SAM WHITE',
-        description: 'This place is definitely worth visiting and making friends there. I dont know how you do it, but it is a strange combination of a posh place for beer snobs and a local pub at the same time. It`s so weird that it works. Love it!',
-        date: 'May 28, 2022'
-    },
-    {
-        id: '2',
-        image: 'img/testimonials/profile-bernard.webp',
-        name: 'BERNARD REDHEAD',
-        description: 'I am definitely a beer person, and my girlfriend loves cider offered at Beer Boutique as well. We love to taste different craft beer varieties on Sundays here. Besides, the concerts there are amazing. Rock it on!',
-        date: 'March 15, 2022'
-    },
-
-];
+const response = await fetch('api/testimonials.json');
+const testimonials = await response.json()
 
 function renderTestimonials(testimonials) {
     let testimonialsDomString = '';
     for (const testimonial of testimonials) {
-        testimonialsDomString += ` 
+        testimonialsDomString += `
         <div class="testimonials__review-card">
         <div class="testimonials__review">
         <img
