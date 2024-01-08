c737718bfc8f47298df5afed449e6abc442b1bbf
Zhecka1992 <Bondarenkoyy@gmail.com>
2024-01-08 17:04:01 +0200

    Change our story

--- js/button-story.js
+++ js/button-story.js
@@ -2,10 +2,10 @@ document.addEventListener('DOMContentLoaded', function() {
   var storyButton = document.querySelector('.story-button');
 
   storyButton.addEventListener('mouseover', function() {
-    this.style.backgroundColor = '#cc8400'; // Darker shade for the hover state
+    this.style.backgroundColor = '#cc8400';
   });
 
   storyButton.addEventListener('mouseout', function() {
-    this.style.backgroundColor = '#ffa500'; // Original color
+    this.style.backgroundColor = '#ffa500';
   });
 });
